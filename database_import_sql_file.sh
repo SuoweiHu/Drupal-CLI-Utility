71368326fc2d7b5dea59fe0b29c6630052b7cdd3
Suowei Hu <suowei.h@gmail.com>
2023-11-15 19:18:53 +1100

    Modi: enhance user prompting for database import/export/delete script

--- database_import_sql_file.sh
+++ database_import_sql_file.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Read the datbase setting and store them in variables
+split_line_length=100
 settings=$( echo "sites/default/settings.php" )
 database=$( cat "${settings}" | grep "'database' =>" | grep -v "*" | grep -v "#" | sed -n "s/'database' => '\([^']*\)'.*/\1/p" | sed -e 's/^[[:space:]]*//' )
 username=$( cat "${settings}" | grep "'username' =>" | grep -v "*" | grep -v "#" | sed -n "s/'username' => '\([^']*\)'.*/\1/p" | sed -e 's/^[[:space:]]*//' )
@@ -24,17 +25,41 @@ echo -en " \e[1;31m # Import-cmd:         <\e[1;33m"  && echo -e "\e[1;33    mmy
 echo -en " \e[1;31m # Export-cmd:         <\e[1;33m"  && echo -e "\e[1;33 mysqldump -u $username -p"$password" "$database" > "$file_in_out" \e[0m";
 echo ""
 
-# user input: the type of action
-echo -en " \e[1;31m # Delete-table (y/n): <\e[1;33m"  && read delete_tables
-echo -en " \e[1;31m # Import-table (y/n): <\e[1;33m"  && read import_tables
-echo -en " \e[1;31m # Export-table (y/n): <\e[1;33m"  && read export_tables
-echo ""
+# =================
+# delete database ?
+echo -en " \e[1;31m # Delete-table (y/n): <\e[1;33m"  && read delete_tables && echo -en "\e[0m"
+if [[ "$delete_tables" == "y" ]]; then echo "Deleting  Table: "
+    echo ""
+    printf '%*s\n' "$split_line_length" '' | tr ' ' '-'
+    echo "Deleting Table: "
+    mysql -u "$username" -p"$password" -e "USE $database; SHOW TABLES;" | grep -v Tables_in | while read -r table; do mysql -u "$username" -p"$password" -e "USE $database; DROP TABLE $table;"; done
+    printf '%*s\n' "$split_line_length" '' | tr ' ' '-'
+fi #>/dev/null && fi
 
-# dropping & importing
-query="SELECT CONCAT('DROP TABLE IF EXISTS ', table_name, ';') AS statement FROM information_schema.tables WHERE table_schema = '$database'"
-if [[ "$delete_tables" == "y" ]]; then echo "Deleting  Table: " &&     mysql -u "$username" -p"$password" -N -B -e "$query" "$database"  && fi #>/dev/null && fi
-if [[ "$import_tables" == "y" ]]; then echo "Importing Table: " &&     mysql -u "$username" -p"$password" "$database" < "$file_in_out"   && fi #>/dev/null && fi
-if [[ "$export_tables" == "y" ]]; then echo "Exporting Table: " && mysqldump -u "$username" -p"$password" "$database" > "$file_in_out"   && fi #>/dev/null && fi
+# =================
+# export database ?
+echo -en " \e[1;31m # Export-table (y/n): <\e[1;33m"  && read export_tables && echo -en "\e[0m"
+if [[ "$export_tables" == "y" ]]; then
+    echo ""
+    printf '%*s\n' "$split_line_length" '' | tr ' ' '-'
+    echo "Exporting Table: "
+    mysqldump -u "$username" -p"$password" "$database" > "$file_in_out"
+    printf '%*s\n' "$split_line_length" '' | tr ' ' '-'
+fi #>/dev/null && fi
+
+# =================
+# export database ?
+echo -en " \e[1;31m # Import-table (y/n): <\e[1;33m"  && read import_tables && echo -en "\e[0m"
+if [[ "$import_tables" == "y" ]]; then
+    echo ""
+    printf '%*s\n' "$split_line_length" '' | tr ' ' '-'
+    echo "Importing Table: "
+    mysql -u "$username" -p"$password" "$database" < "$file_in_out"
+    printf '%*s\n' "$split_line_length" '' | tr ' ' '-'
+fi #>/dev/null && fi
+
+# =================
+
 
 # Unset commands reads
 unset settings
