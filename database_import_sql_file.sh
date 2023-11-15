8dc1f165b3a7207735760e5b03e4a86dd04705c5
Suowei Hu <suowei.h@gmail.com>
2023-11-15 19:49:55 +1100

    Revert: resetting (removing the changes for adapting bash)

--- database_import_sql_file.sh
+++ database_import_sql_file.sh
@@ -8,39 +8,26 @@ username=$( cat "${settings}" | grep "'username' =>" | grep -v "*" | grep -v "#"
 password=$( cat "${settings}" | grep "'password' =>" | grep -v "*" | grep -v "#" | sed -n "s/'password' => '\([^']*\)'.*/\1/p" | sed -e 's/^[[:space:]]*//' )
 
 # Check if any of the variable is empty, if yes them hault th program
-if [[ -z "$database" ]]; then
-    echo "Error: Variable 'database' is not set."
-    return
-fi
-if [[ -z "$username" ]]; then
-    echo "Error: Variable 'username' is not set."
-    return
-fi
-if [[ -z "$password" ]]; then
-    echo "Error: Variable 'password' is not set."
-    return
-fi
+if [[ -z "$database" ]]; then echo "Error: Variable 'database' is not set." && return && fi
+if [[ -z "$username" ]]; then echo "Error: Variable 'username' is not set." && return && fi
+if [[ -z "$password" ]]; then echo "Error: Variable 'password' is not set." && return && fi
 
 # Print the results in varaibles (and with color highlighting)
-echo "" && \
-echo "" && \
-echo " \033[0;31m | Settings: [\033[0;33m${settings}\033[0;31m] \033[0m"
-echo " \033[0;31m | Database: [\033[0;33m${database}\033[0;31m] \033[0m"
-echo " \033[0;31m | Username: [\033[0;33m${username}\033[0;31m] \033[0m"
-echo " \033[0;31m | Password: [\033[0;33m${password}\033[0;31m] \033[0m"
-echo "" && \
+echo -en " \e[1;31m | Settings: [\e[1;33m${settings}\e[1;31m] \e[0m\n"
+echo -en " \e[1;31m | Database: [\e[1;33m${database}\e[1;31m] \e[0m\n"
+echo -en " \e[1;31m | Username: [\e[1;33m${username}\e[1;31m] \e[0m\n"
+echo -en " \e[1;31m | Password: [\e[1;33m${password}\e[1;31m] \e[0m\n"
 echo ""
 
 # user input: prompt for user to input the filepath
-# echo " \033[0;31m # Import/Export-file: <\033[0;33m" && read -p "123" file_in_out
-read -rp $'\e[1m'"# Import/Export-file: \033[0;32m" file_in_out$'\e[0m'
-echo " \033[0;31m # Import-cmd:         <\033[0;33m  \033[0;31m mmysql -u $username -p"$password" "$database" < '$file_in_out' \\033[0m";
-echo " \033[0;31m # Export-cmd:         <\033[0;33m  \033[0;31m mysqldump -u $username -p"$password" "$database" > "$file_in_out" \\033[0m";
+echo -en " \e[1;31m # Import/Export-file: <\e[1;33m"  && read file_in_out
+echo -en " \e[1;31m # Import-cmd:         <\e[1;33m"  && echo -e "\e[1;33    mmysql -u $username -p"$password" "$database" < '$file_in_out' \e[0m";
+echo -en " \e[1;31m # Export-cmd:         <\e[1;33m"  && echo -e "\e[1;33 mysqldump -u $username -p"$password" "$database" > "$file_in_out" \e[0m";
 echo ""
 
 # =================
 # delete database ?
-echo " \033[0;31m # Delete-table (y/n): <\033[0;33m"  && read delete_tables && echo "\\033[0m"
+echo -en " \e[1;31m # Delete-table (y/n): <\e[1;33m"  && read delete_tables && echo -en "\e[0m"
 if [[ "$delete_tables" == "y" ]]; then echo "Deleting  Table: "
     echo ""
     printf '%*s\n' "$split_line_length" '' | tr ' ' '-'
@@ -51,7 +38,7 @@ fi #>/dev/null && fi
 
 # =================
 # export database ?
-echo " \033[0;31m # Export-table (y/n): <\033[0;33m"  && read export_tables && echo "\\033[0m"
+echo -en " \e[1;31m # Export-table (y/n): <\e[1;33m"  && read export_tables && echo -en "\e[0m"
 if [[ "$export_tables" == "y" ]]; then
     echo ""
     printf '%*s\n' "$split_line_length" '' | tr ' ' '-'
@@ -62,7 +49,7 @@ fi #>/dev/null && fi
 
 # =================
 # export database ?
-echo " \033[0;31m # Import-table (y/n): <\033[0;33m"  && read import_tables && echo "\\033[0m"
+echo -en " \e[1;31m # Import-table (y/n): <\e[1;33m"  && read import_tables && echo -en "\e[0m"
 if [[ "$import_tables" == "y" ]]; then
     echo ""
     printf '%*s\n' "$split_line_length" '' | tr ' ' '-'
--- database_read_settings.sh
+++ database_read_settings.sh
@@ -7,28 +7,18 @@ username=$( cat "${settings}" | grep "'username' =>" | grep -v "*" | grep -v "#"
 password=$( cat "${settings}" | grep "'password' =>" | grep -v "*" | grep -v "#" | sed -n "s/'password' => '\([^']*\)'.*/\1/p" | sed -e 's/^[[:space:]]*//') ;
 
 # Check if any of the variable is empty, if yes them hault th program
-if [[ -z "$database" ]]; then
-    echo "Error: Variable 'database' is not set."
-    return
-fi
-if [[ -z "$username" ]]; then
-    echo "Error: Variable 'username' is not set."
-    return
-fi
-if [[ -z "$password" ]]; then
-    echo "Error: Variable 'password' is not set."
-    return
-fi
+if [[ -z "$database" ]]; then echo "Error: Variable 'database' is not set." && return && fi
+if [[ -z "$username" ]]; then echo "Error: Variable 'username' is not set." && return && fi
+if [[ -z "$password" ]]; then echo "Error: Variable 'password' is not set." && return && fi
 
 # Print the results in varaibles (and with color highlighting)
-echo "" && \
-echo "" && \
-echo " \033[0;31m | Settings: [\033[0;33m${settings}\033[0;31m] \\033[0m" && \
-echo " \033[0;31m | Database: [\033[0;33m${database}\033[0;31m] \\033[0m" && \
-echo " \033[0;31m | Username: [\033[0;33m${username}\033[0;31m] \\033[0m" && \
-echo " \033[0;31m | Password: [\033[0;33m${password}\033[0;31m] \\033[0m" && \
-echo "" && \
-echo ""
+echo -en "\n \e[1;31m | Settings: [\e[1;33m${settings}\e[1;31m] \e[0m" && \
+echo -en "\n \e[1;31m | Database: [\e[1;33m${database}\e[1;31m] \e[0m" && \
+echo -en "\n \e[1;31m | Username: [\e[1;33m${username}\e[1;31m] \e[0m" && \
+echo -en "\n \e[1;31m | Password: [\e[1;33m${password}\e[1;31m] \e[0m" && \
+echo -en "\n" && \
+echo -en "\n" && \
+echo -en "\n";
 
 # Unset commands reads
 unset settings
